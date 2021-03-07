08149c74f941db92fe15dbe305cb46e9733223b0
elishaaquino <elishaaquino1998@gmail.com>
2021-03-07 15:57:05 -0800

    took out stuff

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,8 +1,6 @@
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
-
-import scala.collection.mutable
 import scala.io.Source
 
 object App {
@@ -15,32 +13,14 @@ object App {
     val conf = new SparkConf().setAppName("Lab6").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    val tvActors = mutable.ListBuffer[String]()
-    val tvActorsRating = mutable.Map[String, (Double, Int)]()
-
     val tvShows = Source.fromFile("netflixIMDB.csv").getLines.toList
       .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(1).equals("TV Show"))
       .filter(line => !line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4).equals("bam"))
       .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(5).contains("United States"))
-      .foreach(line =>
-        {
-          val rating = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(8)
-          line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4).split(",")
-            .foreach(actor => tvActors += actor.trim().replace("\"", "") + "," + rating)
-        })
-
-    tvActors.foreach(actorRating =>
-      if (tvActorsRating.contains(actorRating.split(",")(0)))
-        tvActorsRating(actorRating.split(",")(0)) = (actorRating.split(",")(1).toDouble
-          + tvActorsRating(actorRating.split(",")(0))._1, tvActorsRating(actorRating.split(",")(0))._2 + 1)
-      else
-        tvActorsRating.put(actorRating.split(",")(0), (actorRating.split(",")(1).toDouble, 1))
-    )
-
-    // prints (name, (avgrating, # of shows))
-    tvActorsRating.map(x => (x._1, (x._2._1/x._2._2, x._2._2))).foreach(println(_))
-
-
-
+      .map(line => (line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4),
+        line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(8)))
+      .map(actorRating => actorRating._1.split(",").map(actor =>
+        (actor.trim().replace("\"", ""), actorRating._2)))
+      .map(x => x.foreach(println(_)))
   }
 }
