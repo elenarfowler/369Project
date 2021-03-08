098aeba1d62384af5dfda764f12ed029e549ec9b
elenarfowler <elenarfowler@gmail.com>
2021-03-08 14:18:04 -0800

    find average ratings, sort by rating, and take top 10 for tv shows and movies

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -13,14 +13,45 @@ object App {
     val conf = new SparkConf().setAppName("Lab6").setMaster("local[4]")
     val sc = new SparkContext(conf)
 
-    val tvShows = Source.fromFile("netflixIMDB.csv").getLines.toList
+    val tvShows = sc.textFile("netflixIMDB.csv")
       .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(1).equals("TV Show"))
       .filter(line => !line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4).equals("bam"))
       .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(5).contains("United States"))
       .map(line => (line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4),
         line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(8)))
-      .map(actorRating => actorRating._1.split(",").map(actor =>
-        (actor.trim().replace("\"", ""), actorRating._2)))
-      .map(x => x.foreach(println(_)))
+      .flatMap({case (key, value) => key.split(",")
+        .map(actor => (actor.trim().replace("\"", ""), value.toDouble))})
+      .groupByKey()
+      .map({case (key, value) => (key, value.aggregate((0.0, 0.0))(
+        (x,y) => (x._1 + y, x._2 + 1),
+        (x,y) => (x._1 + y._1, x._2 + y._2)
+      ))})
+      .map({case (key, value) => (value._1/value._2, key)})
+      .sortByKey(false)
+      .collect()
+      .map({case (key, value) => (value, key)})
+      .take(10)
+      .foreach(println(_))
+
+    val movie = sc.textFile("netflixIMDB.csv")
+      .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(1).equals("Movie"))
+      .filter(line => !line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4).equals("bam"))
+      .filter(line => line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(5).contains("United States"))
+
+      .map(line => (line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(4),
+        line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1)(8)))
+      .flatMap({case (key, value) => key.split(",")
+        .map(actor => (actor.trim().replace("\"", ""), value.toDouble))})
+      .groupByKey()
+      .map({case (key, value) => (key, value.aggregate((0.0, 0.0))(
+        (x,y) => (x._1 + y, x._2 + 1),
+        (x,y) => (x._1 + y._1, x._2 + y._2)
+      ))})
+      .map({case (key, value) => (value._1/value._2, key)})
+      .sortByKey(false)
+      .collect()
+      .map({case (key, value) => (value, key)})
+      .take(10)
+      .foreach(println(_))
   }
 }
